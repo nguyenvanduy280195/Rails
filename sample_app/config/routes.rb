60666e3a967fd9c8fb00dc20f03d2850afd3c941
Nguyễn Văn Duy <nguyenvanduy280195@gmail.com>
2017-04-23 13:49:26 +0700

    Complete chapter 7

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   # get '/home',    to: 'static_pages/home'
   root 'static_pages#home'
   get '/help',    to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   get '/about',   to: 'static_pages#about'
   get '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
